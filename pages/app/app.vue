43ac7fe572c2e6a3263634ce560b787cc2c58b9e
MAC <mac@MACui-MacBook-Pro.local>
2017-12-17 03:42:02 +0900

    no message

--- pages/app/app.vue
+++ pages/app/app.vue
@@ -1,3 +1,5 @@
 <template>
 <h3>설치페이지</h3>
+<a href="https://play.google.com/store/apps/details?id=com.whoclient"><button value="안드로이드마켓"></a>
+<a href="javascript:alert('준비중');"><button value="앱스토어"></a>
 </template>
