9fa1945ee50e8ba6063973bee5336f5249454240
Choi Iris <draconids@nate.com>
2018-07-12 18:59:41 +0900

    feat: add title and change ios link

--- pages/app/app.vue
+++ pages/app/app.vue
@@ -1,13 +1,19 @@
 <template>
-    <div style="display: table; margin: 0 auto;">
     <div>
-    <a href="https://play.google.com/store/apps/details?id=com.whoclient" title="안드로이드마켓"><img src="~/assets/applinkg_150.png"></a>
-    </div>
-    <div style="padding-top: 30px;">
-    <a href="https://itunes.apple.com/us/app/%EB%88%84%EA%B5%B4%EA%B9%8C/id1288404693?l=ko&ls=1&mt=8" title="앱스토어"><img src="~/assets/applinka_150.png"></a>
-    </div>
-    <div style="padding-top: 30px;">
-    <a href="http://nuguga.com" title="홈페이지"><img src="~/assets/applinkh_150.png"></a>
-    </div>
+        <div style="display: table; margin: 0 auto; font-family: Roboto, sans-serif;">
+            <h4>누굴까는 내가 아는 사람과 익명으로 채팅하는 메신저입니다.</h4>
+            <h4>(손가락으로 화면을 좌우로 벌리면 좀 더 크게 보실 수 있습니다)</h4>
+        </div>
+        <div style="display: table; margin: 0 auto;">
+            <div style="padding-top: 30px;">
+                <a href="https://play.google.com/store/apps/details?id=com.whoclient" title="안드로이드마켓"><img src="~/assets/applinkg_150.png"></a>
+            </div>
+            <div style="padding-top: 30px;">
+                <a href="https://tunes.apple.com/kr/app/%EB%88%84%EA%B5%B4%EA%B9%8C/id1288404693?mt=8" title="앱스토어"><img src="~/assets/applinka_150.png"></a>
+            </div>
+            <div style="padding-top: 30px;">
+                <a href="http://nuguga.com" title="홈페이지"><img src="~/assets/applinkh_150.png"></a>
+            </div>
+        </div>
     </div>
 </template>
