1603f521a4440f222740c5d3c369b7b9f0259c67
Choi Iris <draconids@nate.com>
2018-07-12 19:09:34 +0900

    chore: fix typo

--- pages/app/app.vue
+++ pages/app/app.vue
@@ -9,7 +9,7 @@
                 <a href="https://play.google.com/store/apps/details?id=com.whoclient" title="안드로이드마켓"><img src="~/assets/applinkg_150.png"></a>
             </div>
             <div style="padding-top: 30px;">
-                <a href="https://tunes.apple.com/kr/app/%EB%88%84%EA%B5%B4%EA%B9%8C/id1288404693?mt=8" title="앱스토어"><img src="~/assets/applinka_150.png"></a>
+                <a href="https://itunes.apple.com/kr/app/%EB%88%84%EA%B5%B4%EA%B9%8C/id1288404693?mt=8" title="앱스토어"><img src="~/assets/applinka_150.png"></a>
             </div>
             <div style="padding-top: 30px;">
                 <a href="http://nuguga.com" title="홈페이지"><img src="~/assets/applinkh_150.png"></a>
